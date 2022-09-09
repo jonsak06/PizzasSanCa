e3b20bdc623d007c52b8424cbe0c2a9e0dd731ec
Porifly <ataqueomega07@gmail.com>
2022-09-09 20:00:14 -0300

    Persistencia Leo

---
+++ PizzasSanCa/src/Persistencia/PersistenciaMateriales.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PersistenciaMateriales {
+
+    private PersistenciaMateriales() {
+    }
+
+    
+   public static PersistenciaMateriales getInstance() {
+        return PersistenciaMaterialesHolder.INSTANCE;
+    }
+
+    private static class PersistenciaMaterialesHolder {
+
+        private static final PersistenciaMateriales INSTANCE = new PersistenciaMateriales();
+        private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ANIMALITOS_VETPU");
+        private static final EntityManager em = emf.createEntityManager();
+
+    }
+
+    public EntityManager getEntity() {
+        return PersistenciaMaterialesHolder.em;
+    }
+
+    public void persist(Object object) {
+        EntityManager em = getEntity();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+
+    public void refresh(Object object) {
+        EntityManager em = getEntity();
+        em.getTransaction().begin();
+        try {
+            em.refresh(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+
+    public void merge(Object object) {
+        EntityManager em = getEntity();
+        em.getTransaction().begin();
+        try {
+            em.merge(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+
+    public void delete(Object object) {
+        EntityManager em = getEntity();
+        em.getTransaction().begin();
+        try {
+            em.remove(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+}
